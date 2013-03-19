aba9d570196f3e0464392afe9e376131d2b2ad0b
bonjinkoo <bjkoo@berkeley.edu>
2013-03-19 00:57:25 -0700

    updating Board.java

--- player/Board.java
+++ player/Board.java
@@ -13,10 +13,30 @@ public class Board {
 	private int[][][] board;
 	
 	
-	//Constructs an 8x8 board.
+	// Constructs an 8x8 board.
 	public Board() {
 		board = new int[8][8][1];
 	}
+	
+	// Returns whether or not the move involves a corner on the Board. 
+	// If it is a corner, returns true. Else, returns false.
+	public boolean ifCorner(Move m) {
+		if ((m.x1 + m.x2) == 00) {
+			return true;
+		}
+		else if ((m.x1 + m.x2) == 70) {
+			return true;
+		}
+		else if ((m.x1 + m.x2) == 07) {
+			return true;
+		}
+		else if ((m.x1 + m.x2) == 77) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	
 	
 }
