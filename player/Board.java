51c29b5e5f1d850cb171b00b8add991c67827fe7
bonjinkoo <bjkoo@berkeley.edu>
2013-03-19 01:27:03 -0700

    updating Board

--- player/Board.java
+++ player/Board.java
@@ -1,8 +1,8 @@
 package player;
 
 /**
- * A public class for constructing and holding the actual game board object.
- * Add more description?
+ * A public class for constructing and holding the actual game board object. Add
+ * more description?
  */
 
 public class Board {
@@ -11,32 +11,51 @@ public class Board {
 	final static int WHITE = 1;
 	final static int BLACK = 2;
 	private int[][][] board;
-	
-	
+
 	// Constructs an 8x8 board.
 	public Board() {
 		board = new int[8][8][1];
 	}
-	
-	// Returns whether or not the move involves a corner on the Board. 
+
+	// Returns whether or not the move involves a corner on the Board.
 	// If it is a corner, returns true. Else, returns false.
 	public boolean ifCorner(Move m) {
 		if ((m.x1 + m.x2) == 00) {
 			return true;
-		}
-		else if ((m.x1 + m.x2) == 70) {
+		} else if ((m.x1 + m.x2) == 70) {
 			return true;
-		}
-		else if ((m.x1 + m.x2) == 07) {
+		} else if ((m.x1 + m.x2) == 07) {
 			return true;
-		}
-		else if ((m.x1 + m.x2) == 77) {
+		} else if ((m.x1 + m.x2) == 77) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
+		
 	
 	
+	
+	// CODE FOR DEBUGGING 
+
+	// FOR TESTING. CHECKING CELL CONTENTS OF BOARD.
+	public int cellContents(int x, int y) {
+		return board[x][y][0];
+	}
+
+	// FOR TESTING. MANUAL ADD.
+	public void addWhitePiece(int x, int y) {
+		if (board[x][y][0] == EMPTY) {
+			board[x][y][0] = WHITE;
+		}
+	}
+
+	// FOR TESTING. MANUAL ADD.
+	public void addBlackPiece(int x, int y) {
+		if (board[x][y][0] == EMPTY) {
+			board[x][y][0] = BLACK;
+		}
+	}
+
 }
