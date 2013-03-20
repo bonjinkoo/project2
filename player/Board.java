df9e0a7a3af6deb4fc5391b65424b5ca3e654b0a
bonjinkoo <bjkoo@berkeley.edu>
2013-03-20 01:33:21 -0700

    updating Board and Tester

--- player/Board.java
+++ player/Board.java
@@ -1,5 +1,7 @@
 package player;
 
+import list.DList;
+
 /**
  * A public class for constructing and holding the actual game board object. Add
  * more description?
@@ -7,54 +9,152 @@ package player;
 
 public class Board {
 
+	final static int SIZE = 8;
 	final static int EMPTY = 0;
 	final static int WHITE = 1;
 	final static int BLACK = 2;
-	private int[][][] board;
+	private int[][] board;
 
 	// Constructs an 8x8 board.
 	public Board() {
-		board = new int[8][8][1];
+		board = new int[SIZE][SIZE];
 	}
 
 	// Returns whether or not the move involves a corner on the Board.
 	// If it is a corner, returns true. Else, returns false.
 	public boolean ifCorner(Move m) {
-		if ((m.x1 + m.x2) == 00) {
+		if ((m.x1 + m.y1) == 00) {
 			return true;
-		} else if ((m.x1 + m.x2) == 70) {
+		} else if ((m.x1 + m.y1) == 70) {
 			return true;
-		} else if ((m.x1 + m.x2) == 07) {
+		} else if ((m.x1 + m.y1) == 07) {
 			return true;
-		} else if ((m.x1 + m.x2) == 77) {
+		} else if ((m.x1 + m.y1) == 77) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-		
-	
-	
-	
-	// CODE FOR DEBUGGING 
+
+	// Returns a list of all possible connections between the input piece
+	// and ONE other piece.
+	public DList singleConnections(int x, int y) {
+		DList connections = new DList();
+		connections.insertBack(x + y);
+		int color = board[x][y];
+		if (board[x][y] != EMPTY) {
+			if (board[x][y] == color) {
+				// left
+				for (int i = x; x >= 0; i--) {
+					if (board[i][y] == color) {
+						connections.insertBack(i + y);
+						break;
+					} else if (board[i][y] != color) {
+						break;
+					}
+				}
+				// right
+				for (int i = x; x <= 7; i++) {
+					if (board[i][y] == color) {
+						connections.insertBack(i + y);
+						break;
+					} else if (board[i][y] != color) {
+						break;
+					}
+				}
+				// up
+				for (int i = y; y >= 0; i--) {
+					if (board[x][i] == color) {
+						connections.insertBack(x + i);
+						break;
+					} else if (board[x][i] != color) {
+						break;
+					}
+				}
+				// down
+				for (int i = y; y <= 7; i++) {
+					if (board[x][i] == color) {
+						connections.insertBack(x + i);
+						break;
+					} else if (board[x][i] != color) {
+						break;
+					}
+				}
+				// upper left
+				int j = x;
+				for (int i = y; y >= 0; i--) {
+					if (board[j][i] == color) {
+						connections.insertBack(j + i);
+						break;
+					} else if (board[j][i] != color) {
+						break;
+					} else if (j < 0 || i < 0) {
+						break;
+					}
+					j -= 1;
+				}
+				// upper right
+				j = x;
+				for (int i = y; y >= 0; i--) {
+					if (board[j][i] == color) {
+						connections.insertBack(j + i);
+						break;
+					} else if (board[j][i] != color) {
+						break;
+					} else if (j < 0 || i < 0) {
+						break;
+					}
+					j += 1;
+				}
+				// lower left
+				j = x;
+				for (int i = y; y <= 7; i++) {
+					if (board[j][i] == color) {
+						connections.insertBack(j + i);
+						break;
+					} else if (board[j][i] != color) {
+						break;
+					} else if (j < 0 || i < 0) {
+						break;
+					}
+					j -= 1;
+				}
+				// lower right
+				j = x;
+				for (int i = y; y <= 7; i++) {
+					if (board[j][i] == color) {
+						connections.insertBack(j + i);
+						break;
+					} else if (board[j][i] != color) {
+						break;
+					} else if (j < 0 || i < 0) {
+						break;
+					}
+					j += 1;
+				}
+			}
+		}
+		return connections;
+	}
+
+	// CODE FOR DEBUGGING
 
 	// FOR TESTING. CHECKING CELL CONTENTS OF BOARD.
 	public int cellContents(int x, int y) {
-		return board[x][y][0];
+		return board[x][y];
 	}
 
 	// FOR TESTING. MANUAL ADD.
 	public void addWhitePiece(int x, int y) {
-		if (board[x][y][0] == EMPTY) {
-			board[x][y][0] = WHITE;
+		if (board[x][y] == EMPTY) {
+			board[x][y] = WHITE;
 		}
 	}
 
 	// FOR TESTING. MANUAL ADD.
 	public void addBlackPiece(int x, int y) {
-		if (board[x][y][0] == EMPTY) {
-			board[x][y][0] = BLACK;
+		if (board[x][y] == EMPTY) {
+			board[x][y] = BLACK;
 		}
 	}
 
--- player/Tester.java
+++ player/Tester.java
@@ -3,11 +3,14 @@ package player;
 public class Tester {
 
 	public static void main(String args[]) {
+		/*
 		Board testB = new Board();
 		System.out.println(testB.cellContents(0, 0));
 		testB.addBlackPiece(1, 0);
 		testB.addWhitePiece(2, 0);
 		System.out.println(testB.cellContents(1, 0));
 		System.out.println(testB.cellContents(2, 0));
+		*/
+		
 	}
 }
